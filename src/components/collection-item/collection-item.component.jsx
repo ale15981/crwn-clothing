eb47b9a04e116e4f16e28e2bf13ae99ee68cd8f0
Alessandro Casamassima <alessandro.casamassima@gmail.com>
2020-08-17 21:06:05 +0200

    adding styled-components, converted homepage, header and custom button over to styled-components

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -9,18 +9,22 @@ import "./collection-item.styles.scss";
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
   return (
-    <div className='collection-item'>
+    <div className="collection-item">
       <div
-        className='image'
+        className="image"
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+      <CustomButton
+        className="custom-button"
+        onClick={() => addItem(item)}
+        inverted
+      >
         {" "}
         Add to cart{" "}
       </CustomButton>
